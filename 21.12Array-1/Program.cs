3374a13d0461e038220c6a43c6900ea1401058bc
Nikaragis <nikolaj@iMac-Nikolaj.local>
2021-12-23 19:25:39 +0300

    Add homeworks from 17.12 to 21.12

---
+++ 21.12Array-1/Program.cs
@@ -0,0 +1,57 @@
+﻿
+
+
+
+
+
+/* Реализуйте задачу 1 в виде статического класса StaticClass;
+а) Класс должен содержать статический метод, который принимает на вход массив и решает задачу 1;
+*/
+using System;
+
+namespace numb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random newRand = new Random();
+            int amount = 0;
+            int value = 20;
+            int[] array = new int[value];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = newRand.Next(-10000, 10000);
+                Console.WriteLine(array[i]);
+            }
+
+
+
+            Console.WriteLine($"pair mount = {StaticClass.Solve(array)}");
+            Console.ReadLine();
+        }
+    }
+
+    static class StaticClass
+    {
+        public static int Solve(int[] array)
+        {
+            Random newRand = new Random();
+            int amount = 0;
+            int value = 20;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] % 3 == 0 && array[i + 1] % 3 != 0)
+                {
+                    amount++;
+                }
+                else if (array[i + 1] % 3 == 0 && array[i] % 3 != 0)
+                {
+                    amount++;
+                }
+            }
+            return amount;
+        }
+    }
+}
---
+++ 21.12Array/Program.cs
@@ -0,0 +1,52 @@
+﻿
+/*1.	Дан  целочисленный  массив  из 20 элементов. 
+ * Элементы  массива  могут принимать  целые  значения  от –10 000 до 10 000 включительно.
+ * Заполнить случайными числами.  
+ * Написать программу, позволяющую найти и вывести количество пар элементов массива, в которых только одно число делится на 3. 
+ * В данной задаче под парой подразумевается два подряд идущих элемента массива.
+ * Например, для массива из пяти элементов: 6; 2; 9; –3; 6 ответ — 2. 
+ * 
+ */
+
+using System;
+using System.IO;
+
+namespace _2212Massiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random newRand = new Random();
+            int amount = 0;
+            int value = 20;
+            //string data = File.ReadAllText(@"");
+            //int a = int.Parse(data);
+
+            int[] array = new int[value];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = newRand.Next(-10000, 10000);
+                Console.WriteLine(array[i]);
+            }
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] % 3 == 0 && array[i + 1] % 3 != 0)
+                {
+                    amount++;
+                }
+                else if (array[i + 1] % 3 == 0 && array[i] % 3 != 0)
+                {
+                    amount++;
+                }
+
+
+            }
+
+            Console.WriteLine($"pair mount = {amount}");
+            Console.ReadLine();
+        }
+    }
+}
+
